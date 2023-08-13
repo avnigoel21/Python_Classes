e4559866cba97c01199d6e04536d56320dbd89b4
Avni Goel <avnigoel98@gmail.com>
2023-08-13 21:31:37 +0530

    working on strings

---
+++ Session_3/homework.py
@@ -0,0 +1,8 @@
+# Write a program to fill in a letter template given below with name and date
+
+letter = '''
+Dear <|NAME|>, 
+    You are selected!
+<|DATE|>
+'''
+print(letter)
--- Session_3/strings.py
+++ Session_3/strings.py
@@ -10,9 +10,42 @@
 s1 = "Aditi is a good dancer"
 # print(s1[5:11])
 
-print(s1[0])
-s1[0] = "S"  # does not work (through an error) -> in strings - you cannot update/change the value/character using indexing
-print(s1[0])
+# print(s1[0])
+# s1[0] = "S"  # does not work (through an error) -> in strings - you cannot update/change the value/character using indexing
+# print(s1[0])
+
+
+# string functions
+
+name = "Aditi is a good dancer"
+# print(len(name)) # returns the length of the string
+
+
+# email = "avni@gmail.com"
+# print(email.endswith(".com"))
+# print(name.startswith("Aditi"))
+
+
+# print(name.count("z"))
+# print(name)
+# print(name.capitalize())
+
+# print(name.find("is"))
+
+# print(name.replace("A" , "S"))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
